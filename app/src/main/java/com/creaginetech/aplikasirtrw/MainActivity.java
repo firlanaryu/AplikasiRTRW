f487d4e81bc8d98b8356ef40ae3ea1a783b6b2bb
firlanaryu <firlanasanur@gmail.com>
2019-02-20 14:09:57 +0700

    Add Main Activity

--- app/src/main/java/com/creaginetech/aplikasirtrw/MainActivity.java
+++ app/src/main/java/com/creaginetech/aplikasirtrw/MainActivity.java
@@ -2,12 +2,28 @@ package com.creaginetech.aplikasirtrw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+
+        return true;
     }
 }
