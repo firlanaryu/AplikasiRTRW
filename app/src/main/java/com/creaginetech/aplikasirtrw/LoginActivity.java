b8db4bc24865559c7be5861d8b9c31176aa8c878
firlanaryu <firlanasanur@gmail.com>
2019-02-22 16:05:39 +0700

    Tambah Anggota Keluarga activity

--- app/src/main/java/com/creaginetech/aplikasirtrw/LoginActivity.java
+++ app/src/main/java/com/creaginetech/aplikasirtrw/LoginActivity.java
@@ -20,7 +20,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,TambahDataKKActivity.class);
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                 startActivity(intent);
             }
         });
