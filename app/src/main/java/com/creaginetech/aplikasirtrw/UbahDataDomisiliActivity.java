f8f38c6895a3d8258bdf5d20a08999e739584734
firlanaryu <firlanasanur@gmail.com>
2019-02-22 16:48:53 +0700

    Ubah Data Domisili activity

---
+++ app/src/main/java/com/creaginetech/aplikasirtrw/UbahDataDomisiliActivity.java
@@ -0,0 +1,13 @@
+package com.creaginetech.aplikasirtrw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UbahDataDomisiliActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_data_domisili);
+    }
+}
